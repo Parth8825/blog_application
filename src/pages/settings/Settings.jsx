02ee174fdaa80d0b479989ece0dab6fcc9efd0e7
Parth Darji <pdarji8825@conestogac.on.ca>
2022-09-07 13:13:18 -0400

    settings and login pages created

--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -1,9 +1,33 @@
 import "./settings.css";
+import Sidebar from "../../components/sidebar/Sidebar";
 
 export default function Settings() {
   return (
     <div className="settings">
-      <div className="settingWrapper"></div>
+      <div className="settingsWrapper">
+        <div className="settingsTitle">
+          <span className="settingsUpdateTitle">Update Your Account</span>
+          <span className="settingsUpdateTitle">Delete Account</span>
+        </div>
+        <form className="settingsForm">
+          <label>Profile Picture</label>
+          <div className="settingsPP">
+            <img src="/images/background/singleBackground.jpg" alt="" />
+            <label htmlFor="fileInput">
+              <i className="settingsPPIcon fa-regular fa-circle-user"></i>
+            </label>
+            <input type="file" id="fileInput" style={{ display: "none" }} />
+          </div>
+          <label>Username</label>
+          <input type="text" placeholder="Parth" />
+          <label>Email</label>
+          <input type="email" placeholder="parth@gmail.com" />
+          <label>Password</label>
+          <input type="password" />
+          <button className="settingsSubmit">Update</button>
+        </form>
+      </div>
+      <Sidebar />
     </div>
   );
 }
