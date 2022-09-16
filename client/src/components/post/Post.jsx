037fe8e125ddaa338115970676dfc6ff73b4b624
Parth Darji <pdarji8825@conestogac.on.ca>
2022-09-15 20:02:01 -0400

    changed links

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function Post({ post }) {
   //Public Folder
-  const PF = "http://localhost:5000/images/";
+  const PF = "https://parthblogapp.herokuapp.com/images/";
   return (
     <div className="post">
       {post.photo && <img className="postImg" src={PF + post.photo} alt="" />}
