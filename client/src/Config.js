eb7041a903121a47abeee5d6dd78e8d2837a4071
Parth Darji <pdarji8825@conestogac.on.ca>
2022-09-16 15:35:03 -0400

    added api in router

--- client/src/Config.js
+++ client/src/Config.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://parthblogapp.herokuapp.com/"
+  baseURL: "https://parthblogapp.herokuapp.com/api/"
 });
